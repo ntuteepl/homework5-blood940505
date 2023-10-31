feef2d43fcbbb69111814591fe74fd2340dba986
blood940505 <146065313+blood940505@users.noreply.github.com>
2023-10-31 13:42:06 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float t;
+    double m;
+    scanf("%f",&t);
+    if(t <= 800){
+        m = t*0.9;
+    }
+    if(t >800 && t <1500){
+        m = (t*0.9)*0.9;
+    }
+    if(t >= 1500){
+        m = t*0.9*0.79;
+    }
+    printf("%.1f",m);
+}
 

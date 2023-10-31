260bd52e6741c7d3b8737e7c04d71726cd0eb55b
blood940505 <146065313+blood940505@users.noreply.github.com>
2023-10-31 13:49:01 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int t,tm;
+    double m;
+    scanf("%d %d",&t,&tm);
+    if(t <= 60){
+        m = t*tm;
+    }
+    if(t >60 && t <= 120){
+        m = (t-60)*tm*1.33 + tm*60;
+    }
+    if(t > 120){
+        m = (t-120)*tm*1.66 + tm*60 + tm*60*1.33;
+    }
+    printf("%.1f",m);
+}
 
